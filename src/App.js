f6f193ce66a276f19f1cfdecd52062f03b677aae
abdallahsekrafi <abdalllah.sek@gmail.com>
2023-09-29 08:47:26 +0100

    Add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//transform the App.js into a class-based component
+class App extends React.Component {
+  //the App Component constructor
+  constructor(props) {
+    super(props)
+    //the App state {Person and show}
+    this.state = {
+      Person: {
+        fullName: "Sekrafi",
+        bio: "123",
+        imgSrc: "https://static.vecteezy.com/system/resources/previews/002/002/403/large_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+        profession: "Full Stak JS Dev"
+      },
+      show: true,
+      count: 0
+    };
+  }
+
+  toggleState = () => {
+    this.setState({
+      // togle the show state between true and false
+      show: !this.state.show
+    });
+  }
+
+  componentDidMount() {
+    // w use setInterval to calculate the time interval when component is mounting
+    this.timerID = setInterval(() => {
+      this.setState({
+        count: this.count + 1
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    // w clear the timerID when component is Unmounting
+    clearInterval(this.timerID);
+    this.setState({
+      count: 0
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.toggleState}>Click me</button> {/* toggleState is a props function */}
+          <div>Time interval: <span className="interval">{this.timerID ? this.timerID : "--"}</span></div>
+          {
+            this.state.show ?
+              /* if show state is true (Person profile appear)*/
+              <div>
+                <img src={this.state.Person.imgSrc} className="App-logo" alt="logo" />
+                <div>
+                  Full Name: <span>{this.state.Person.fullName}</span>
+                </div>
+                <div>
+                  Bio: <span>{this.state.Person.bio}</span>
+                </div>
+                <div>
+                  Profession: <span>{this.state.Person.profession}</span>
+                </div>
+              </div>
+              :
+              <div>
+                {/* if show state is false */}
+              </div>
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
